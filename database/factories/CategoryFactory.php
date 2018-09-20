7103fba366e9e100f0abc46c7ce1570112e32cc2
Neon Emmanuell <360crest@gmail.com>
2018-09-20 15:02:08 +0100

    Adding Factories and seeders

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Model\Category;
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Category::class, function (Faker $faker) {
+    $word = $faker->word;
     return [
-        //
+        'name' => $word,
+        'slug' => str_slug($word)
     ];
 });
